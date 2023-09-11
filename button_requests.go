a98d7bc4671e6a2bc4d66a05151529edd115c13a
Thomas Kosiewski <thoma471@googlemail.com>
2023-09-11 22:05:42 +0200

    Renamed button requests to standard requests
    Signed-off-by: Thomas Kosiewski <thoma471@googlemail.com>

--- button_requests.go
+++ button_requests.go
@@ -1,10 +1,10 @@
 package licenseapi
 
-// ButtonRequestInputFrontEnd is the standard input request payload for the front end -- this is
+// StandardRequestInputFrontEnd is the standard input request payload for the front end -- this is
 // the same (it is embedded) in the StandardRequestInput just without the token auth as the Loft
 // backend will handle adding that for communication to the admin server.
 // +k8s:deepcopy-gen=true
-type ButtonRequestInputFrontEnd struct {
+type StandardRequestInputFrontEnd struct {
 	// ReturnURL is the url that operations should ultimately return to after their operation is
 	// complete. For example, once the license activate process is done, the Loft portal should
 	// redirect to this URL.
@@ -12,21 +12,21 @@ type ButtonRequestInputFrontEnd struct {
 	ReturnURL string `json:"returnURL,omitempty"`
 }
 
-// ButtonRequestInput is a standard payload object used between the Loft front end and the Loft
+// StandardRequestInput is a standard payload object used between the Loft front end and the Loft
 // admin server. It accepts auth information and a URL that is used by the admin server when
 // crafting the redirect link that is returned from "start" type operations (ex. trial activate
 // start that creates a one-time token and returns a url to go to for the front end).
 // +k8s:deepcopy-gen=true
-type ButtonRequestInput struct {
+type StandardRequestInput struct {
 	*InstanceTokenAuth
-	ButtonRequestInputFrontEnd
+	StandardRequestInputFrontEnd
 }
 
-// ButtonRequestOutput is a standard payload object returned by the Loft admin server, it
+// StandardRequestOutput is a standard payload object returned by the Loft admin server, it
 // contains a RedirectURL that encodes a one-time token (if applicable) and a return URL (if
 // applicable) that the front end can then follow to continue the license operation.
 // +k8s:deepcopy-gen=true
-type ButtonRequestOutput struct {
+type StandardRequestOutput struct {
 	// RedirectURL is the URL to redirect to for continuing the license operation (typically stripe
 	// or the loft portal).
 	// +optional
