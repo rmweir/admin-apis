3d33fd658905b32dc83958f0a1115ece1200e38a
Lukas Gentele <17330150+LukasGentele@users.noreply.github.com>
2023-09-27 22:25:35 -0700

    add claims type

--- pkg/licenseapi/auth.go
+++ pkg/licenseapi/auth.go
@@ -8,3 +8,10 @@ type InstanceTokenAuth struct {
 	// Certificate is the signing certificate for the token.
 	Certificate string `json:"certificate" form:"certificate" query:"certificate" validate:"required"`
 }
+
+// +k8s:openapi-gen=true
+// +k8s:deepcopy-gen=true
+type InstanceTokenClaims struct {
+	// Hash is the hash of the payload to be signed with this token
+	Hash string `json:"Hash"`
+}
